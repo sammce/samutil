3004337b6aeb5c5d0122ad67e9df24dad8f16342
Sam McElligott <sammcelligott@outlook.com>
2021-10-04 21:50:20 +0100

    Now console not working

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
     python_requires=">=3",
     entry_points="""
         [console_scripts]
-        samutil=.__cli__:main
+        samutil=__cli__:main
     """,
     author="Sam McElligott",
     keyword="testing, unittesting, formatting, secure, generation, utility",
